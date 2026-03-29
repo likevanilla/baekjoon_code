dd98cbfe785d47b77eaf984bd33de3047471fd62
likevanilla <wjdgur27507@gmail.com>
2026-03-29 17:50:25 +0900

    Javascript: 백준 1927 문제 풀이 코드

---
+++ Javascript/1927/1927.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n").map(Number);
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    this.bubbleUp();
+  }
+
+  pop() {
+    if (this.heap.length === 0) return 0;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.bubbleDown();
+    return min;
+  }
+
+  bubbleUp() {
+    let index = this.heap.length - 1;
+
+    while (index > 0) {
+      const parent = Math.floor((index - 1) / 2);
+
+      if (this.heap[parent] <= this.heap[index]) break;
+
+      [this.heap[parent], this.heap[index]] = [this.heap[index], this.heap[parent]];
+      index = parent;
+    }
+  }
+
+  bubbleDown() {
+    let index = 0;
+    const length = this.heap.length;
+
+    while (true) {
+      let left = index * 2 + 1;
+      let right = index * 2 + 2;
+      let smallest = index;
+
+      if (left < length && this.heap[left] < this.heap[smallest]) {
+        smallest = left;
+      }
+
+      if (right < length && this.heap[right] < this.heap[smallest]) {
+        smallest = right;
+      }
+
+      if (smallest === index) break;
+
+      [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+      index = smallest;
+    }
+  }
+}
+
+const N = input[0];
+const heap = new MinHeap();
+const answer = [];
+
+for (let i = 1; i <= N; i++) {
+  const x = input[i];
+
+  if (x === 0) {
+    answer.push(heap.pop());
+  } else {
+    heap.push(x);
+  }
+}
+
+console.log(answer.join("\n"));
